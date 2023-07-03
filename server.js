e37939c6de13b4e35fe254c75cd0263dad6fa804
ltuckr <l.tucker.rdh@gmail.com>
2023-07-02 20:07:22 -0400

    added routing for api and html

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
-//setting up express connectivity
+//Sets up Express connectivity
 const express = require("express");
 const app = express();
+
+// Parse data input
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// Routes
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
