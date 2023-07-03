6eb975c814520e198afef76ab84fbf527c01b87f
ltuckr <l.tucker.rdh@gmail.com>
2023-07-03 08:40:23 -0400

    added get and post to api

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,3 +4,20 @@ const path = require('path')
 const db = require('./db/db.json')
 
 //Route
+app.get('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', (err, data) => {
+        if (err) throw err;
+        let dbData = JSON.parse(data);
+        res.json(dbData)
+    });   
+})
+//POST 
+app.post('/api/notes', (req, res) => {
+    const newNote = req.body
+   //ID's each note
+    newNote.id = uuidv4()
+    db.push(newNote)
+   //update the json db with new data
+    fs.writeFileSync('./db/db.json', JSON.stringify(db))
+    res.json(db)
+})
