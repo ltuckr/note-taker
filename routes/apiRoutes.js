b9edb0aa48ec0158e7f9238cce62e4dbba08a1b9
ltuckr <l.tucker.rdh@gmail.com>
2023-07-03 14:20:06 -0400

    delete function added

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 const path = require('path')
 const db = require('./db/db.json')
 
-//Route
+//Routing functions
 app.get('/api/notes', (req, res) => {
     fs.readFile('./db/db.json', (err, data) => {
         if (err) throw err;
@@ -11,13 +11,26 @@ app.get('/api/notes', (req, res) => {
         res.json(dbData)
     });   
 })
-//POST 
+
 app.post('/api/notes', (req, res) => {
-    const newNote = req.body
+    const new = req.body
    //ID's each note
-    newNote.id = uuidv4()
-    db.push(newNote)
+    new.id = uuidv4()
+    db.push(new)
    //update the json db with new data
     fs.writeFileSync('./db/db.json', JSON.stringify(db))
     res.json(db)
 })
+
+// DELETE
+app.delete("/api/notes/:id", (req, res) => {
+    let noteId = req.params.id;
+    fs.readFile(__dirname + "/../db/db.json", (err, data) => {
+        if (err) throw err;
+        let notesDB = JSON.parse(data);
+        const filteredNotes = notesDB.filter(values => values.id != noteId);
+        fs.writeFile(__dirname + "/../db/db.json", JSON.stringify(filteredNotes), "utf-8", err => {
+            if (err) throw err;
+            console.log("All done! Your task has been removed.")
+            res.end();
+        });
