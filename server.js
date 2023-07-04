bd5a778d19b954794c0bf135ab8014648db05ff4
ltuckr <l.tucker.rdh@gmail.com>
2023-07-03 21:31:24 -0400

    added updated routing for html and api

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
-// Sets up Express connectivity
+// Sets up Express and routing connectivity
 const express = require("express");
 const app = express();
+const apiRoutes = require("./apiRoutes");
+const htmlRoutes = require("./htmlRoutes");
 
 // Routes
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
 
 // Port info
 const PORT = process.env.PORT || 8080;
